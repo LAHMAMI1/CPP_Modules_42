175a97cd3d96ebb7b75c1b36f7ae3c29dda043c5
Oussama Lahmami <o.lahmami.2000@gmail.com>
2023-12-13 21:20:47 +0100

    CPP06->ex02 (still)

---
+++ CPP_06/ex02/functions.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 21:11:09 by olahmami          #+#    #+#             */
+/*   Updated: 2023/12/13 21:12:29 by olahmami         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base * generate(void);
+void identify(Base* p);
+void identify(Base& p);
