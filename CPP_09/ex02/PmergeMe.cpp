e71df3419086e97cf9b21ff2e8d3ed0afac9c13c
Oussama Lahmami <o.lahmami.2000@gmail.com>
2024-01-31 18:33:37 +0100

    for johnson algo step 1

--- CPP_09/ex02/PmergeMe.cpp
+++ CPP_09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 11:38:40 by olahmami          #+#    #+#             */
-/*   Updated: 2024/01/29 18:05:41 by olahmami         ###   ########.fr       */
+/*   Updated: 2024/01/31 18:30:42 by olahmami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,10 +41,9 @@ void PmergeMe::execution(int ac, char **av)
     for (int i = 1; i < ac; i++)
     {
         arg = av[i];
-        std::cout << arg << std::endl;
         if (arg.empty())
             throw std::invalid_argument("Error: Empty argument");
-        else if (!is_number(arg))
+        else if (!isNumber(arg))
             throw std::invalid_argument("Error: Invalid number");
     }
     
@@ -54,23 +53,41 @@ void PmergeMe::execution(int ac, char **av)
         _deque.push_back(std::atoi(av[i]));
     }
     
-    // std::cout << "Vector contents: ";
-    // for (std::vector<int>::const_iterator i = _vector.begin(); i != _vector.end(); ++i)
-    // {
-    //     std::cout << *i << ' ';
-    // }
-    // std::cout << std::endl;
+    std::cout << "Vector contents before: ";
+    for (std::vector<int>::const_iterator i = _vector.begin(); i != _vector.end(); i++)
+    {
+        std::cout << *i << ' ';
+    }
+    std::cout << std::endl;
 
-    // std::cout << "deque contents: ";
-    // for (std::deque<int>::const_iterator i = _deque.begin(); i != _deque.end(); ++i)
-    // {
-    //     std::cout << *i << ' ';
-    // }
-    // std::cout << std::endl;
+    // ford johnson algorithm
+    // step1: Pairwise comparison
+    std::vector<std::pair<int, int> > pairwise;
+    std::vector<int>::const_iterator next;
+    for (std::vector<int>::const_iterator i = _vector.begin(); i != _vector.end() - 1; i+=2)
+    {
+        
+        next = i + 1;
+        if (next != _vector.end())
+        {
+            if (*i > *next)
+                pairwise.push_back(std::make_pair(*i, *next));
+            else
+                pairwise.push_back(std::make_pair(*next, *i));
+        }
+        
+        
+    }
+    std::cout << "Pairwise vector contents: ";
+    for (std::vector<std::pair<int, int> >::const_iterator i = pairwise.begin(); i != pairwise.end(); i++)
+    {
+        std::cout << "(" << i->first << ", " << i->second << ") ";
+    }
+    std::cout << std::endl;
 }
 
 // checker functions
-bool PmergeMe::is_number(const std::string &arg)
+bool PmergeMe::isNumber(const std::string &arg)
 {
     for (size_t i = 0; i < arg.length(); i++)
         if (!isdigit(arg[i]))
